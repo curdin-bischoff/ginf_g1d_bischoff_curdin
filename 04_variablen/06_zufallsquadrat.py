52f695e73c23bc5e44d113ed70767980e0d6cad9
curdin-bischoff <curdin.bischoff@stud.kswe.ch>
2025-10-10 15:59:16 +0200

    Zufallsquadrat

---
+++ 04_variablen/06_zufallsquadrat.py
@@ -0,0 +1,18 @@
+import turtle
+import random
+
+turtle.shape("turtle")
+x = random.randrange(10, 100)
+
+turtle.fd(x)
+turtle.lt(90)
+turtle.fd(x)
+turtle.lt(90)
+turtle.fd(x)
+turtle.lt(90)
+turtle.fd(x)
+turtle.lt(90)
+
+
+
+
