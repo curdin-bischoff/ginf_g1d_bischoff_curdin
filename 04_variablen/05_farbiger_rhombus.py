f4ee7f187df4e303e77bbb0ec5c7073d66a77c21
curdin-bischoff <curdin.bischoff@stud.kswe.ch>
2025-10-10 15:49:41 +0200

    Farbiger Rhombus

---
+++ 04_variablen/05_farbiger_rhombus.py
@@ -0,0 +1,23 @@
+import turtle
+
+turtle.shape("turtle")
+länge = 100
+farbe = "blue"
+bold = 5
+a = 60
+b = 120
+turtle.pencolor(farbe)
+turtle.pensize(bold)
+
+turtle.rt(30)
+turtle.fd(länge)
+turtle.rt(b)
+turtle.fd(länge)
+turtle.rt(a)
+turtle.fd(länge)
+turtle.rt(b)
+turtle.fd(länge)
+
+
+
+
