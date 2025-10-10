115079d405cfb66d0bfe0ae0e4b874e2f9e539a1
curdin-bischoff <curdin.bischoff@stud.kswe.ch>
2025-10-10 15:39:28 +0200

    Farbiger Stern

---
+++ 04_variablen/04_farbiger_stern.py
@@ -0,0 +1,39 @@
+import turtle
+
+turtle.shape("turtle")
+a = 100
+farbe1 = "red"
+farbe2 = "blue"
+
+turtle.pencolor(farbe1)
+turtle.fd(a)
+turtle.lt(120)
+turtle.fd(a)
+turtle.rt(48)
+
+turtle.pencolor(farbe2)
+turtle.fd(a)
+turtle.lt(120)
+turtle.fd(a)
+turtle.rt(48)
+
+turtle.pencolor(farbe1)
+turtle.fd(a)
+turtle.lt(120)
+turtle.fd(a)
+turtle.rt(48)
+
+turtle.pencolor(farbe2)
+turtle.fd(a)
+turtle.lt(120)
+turtle.fd(a)
+turtle.rt(48)
+
+turtle.pencolor(farbe1)
+turtle.fd(a)
+turtle.lt(120)
+turtle.fd(a)
+turtle.rt(48)
+
+
+
