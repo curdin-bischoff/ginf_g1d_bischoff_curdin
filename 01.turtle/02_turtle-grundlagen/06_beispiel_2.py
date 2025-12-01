eaad09d5399fd96b14b112c9940f7ebe97966608
Oliver Probst <65253973+pro-kswe@users.noreply.github.com>
2025-12-01 23:11:32 +0100

    Typo fixed

--- 01.turtle/02_turtle-grundlagen/06_beispiel_2.py
+++
@@ -1,8 +0,0 @@
-import turtle
-
-turtle.shape("turtle")
-turtle.forward(50)
-turtle.penup()
-turtle.forward(50)
-turtle.pendown()
-turtle.forward(50)
--- 01.turtle/05_schleifen/01_quadrat_loop.py
+++
@@ -1,5 +0,0 @@
-import turtle
-
-for _ in range(4):
-    turtle.fd(100)
-    turtle.lt(90)
