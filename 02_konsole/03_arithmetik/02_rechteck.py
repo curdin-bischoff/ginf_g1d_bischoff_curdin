f399567f73f0c69b4098363214e72813aa9aba42
curdin-bischoff <curdin.bischoff@stud.kswe.ch>
2026-01-06 09:12:05 +0100

    Rechteck

---
+++ 02_konsole/03_arithmetik/02_rechteck.py
@@ -0,0 +1,12 @@
+import random as rd 
+
+a = rd.randrange(100, 1001)
+b = rd.randrange(100, 1001)
+umfang = (a + b) * 2
+fläche = a * b
+print("Berechnung am Rechteck")
+print("Seitenlängen:")
+print(f"a: {a})")
+print(f"b: {b})")
+print(f"Umfang: {umfang}")
+print(f"Flächeninhalt: {fläche}")
