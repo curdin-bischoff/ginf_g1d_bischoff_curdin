ab384ec2e0e64414b616a15128d64845230ed5d5
curdin-bischoff <curdin.bischoff@stud.kswe.ch>
2026-03-09 09:18:45 +0100

    Quadratwurzel

---
+++ 02_konsole/03_arithmetik/06_quadratwurzel.py
@@ -0,0 +1,10 @@
+import math as m
+import random as radien
+ 
+radikanden =  [4, 25, 10, 99, 100, 42, 3600, 289, 2, 7]
+x = rd.choice(radikanden)
+y = m.sqrt(x)
+z = y**2
+print(f"Radikant: {x}")
+print(f"Die Quadratwurzel von {x} ist {y}")
+print(f"Kontrolle: Das QUadrat von {y} ist {z}.")
