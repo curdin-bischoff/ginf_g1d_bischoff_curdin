449fec0389ffe8212df2cc79d62171a5e20a6ece
curdin-bischoff <curdin.bischoff@stud.kswe.ch>
2026-04-27 09:03:53 +0200

    Grundfarben

---
+++ 03_bildbearbeitung/01_grundlagen/02_grundfarben.py
@@ -0,0 +1,8 @@
+import PIL.image as img
+bild = img.new("RGB", (3, 2))
+bild.putpixel((0, 0), (255, 0, 0))
+bild.putpixel((1, 0), (0, 255, 0))
+bild.putpixel((2, 0), (0, 0, 255))
+bild.putpixel((0, 1), (255, 255, 0))
+bild.putpixel((1, 1), (0, 255, 255))
+bild.putpixel((2, 1), (255, 0, 255))
