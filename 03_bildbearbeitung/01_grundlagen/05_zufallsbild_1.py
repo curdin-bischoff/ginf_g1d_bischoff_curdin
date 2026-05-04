5c597daa8a45b9c867e2dd316ef582ccbc62c6db
curdin-bischoff <curdin.bischoff@stud.kswe.ch>
2026-05-04 09:09:12 +0200

    Koorektur Zufallsbild 1

--- 03_bildbearbeitung/01_grundlagen/05_zufallsbild_1.py
+++ 03_bildbearbeitung/01_grundlagen/05_zufallsbild_1.py
@@ -1,12 +1,12 @@
 import PIL.Image as img
 import random as rd
 
-bild = img.new("RGB", (100,100))
+bild = img.new("RGB", (100, 100))
 for _ in range(7500):
     x = rd.randrange(0, 100)
     y = rd.randrange(0, 100)
     r = rd.randrange(0, 256)
-    g = rd.randrange(0,256)
-    b = rd.randrange(0,256)
+    g = rd.randrange(0, 256)
+    b = rd.randrange(0, 256)
     bild.putpixel((x, y), (r, g, b))
 bild.save("05_zufallsbild_1_ergebnis.png")
