ea6d6cbbdc47b8aae2ee9a12767b29ddb5c668bb
curdin-bischoff <curdin.bischoff@stud.kswe.ch>
2026-05-18 08:58:37 +0200

    zufallspixel

---
+++ 03_bildbearbeitung/03_verschachtelte_schleifen/02_zufallspixel.py
@@ -0,0 +1,13 @@
+import PIL.Image as img
+import random as rd 
+
+bild = img.new("RGB", (500,500))
+
+for x in range(0, bild.width):
+    for y in range(0, bild.height):
+        r = rd.randrange(0,256)
+        g = rd.randrange(0,256)
+        b = rd.randrange(0, 256)
+        bild.putpixel((x, y), (r, g, b))
+
+bild.save("01_zufallspixel_ergebnis.png")
