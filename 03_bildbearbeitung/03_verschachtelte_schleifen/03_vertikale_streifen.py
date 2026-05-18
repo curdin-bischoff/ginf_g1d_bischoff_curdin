a86e1c58bbe13f40393e9c0f1a6b293b00de90d1
curdin-bischoff <curdin.bischoff@stud.kswe.ch>
2026-05-18 09:01:43 +0200

    Vertikale streifen

--- 03_bildbearbeitung/03_verschachtelte_schleifen/03_vertikale_streifen.py
+++ 03_bildbearbeitung/03_verschachtelte_schleifen/03_vertikale_streifen.py
@@ -4,10 +4,11 @@ import random as rd
 bild = img.new("RGB", (500,500))
 
 for x in range(0, bild.width):
+    r = rd.randrange(0,256)
+    g = rd.randrange(0,256)
+    b = rd.randrange(0, 256)
+
     for y in range(0, bild.height):
-        r = rd.randrange(0,256)
-        g = rd.randrange(0,256)
-        b = rd.randrange(0, 256)
         bild.putpixel((x, y), (r, g, b))
 
-bild.save("02_zufallspixel_ergebnis.png")
+bild.save("03_vertikale_streifen_ergebnis.png")
