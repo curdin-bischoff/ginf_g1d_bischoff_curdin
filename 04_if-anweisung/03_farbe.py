7b4bcc1f85c94e1291afc359650fadef6c9c6934
curdin-bischoff <curdin.bischoff@stud.kswe.ch>
2026-06-01 09:00:19 +0200

    Farbe

---
+++ 04_if-anweisung/03_farbe.py
@@ -0,0 +1,7 @@
+import random as rd
+
+farbe = input("Bitte geben sie ihre Lieblingsfarbe ein:")
+print(f"Ihre Lieblingsfarbe lautet: {farbe}")
+if farbe == "gelb":
+  print("Eine schöne Farbe")
+print("Vielen Dank")
