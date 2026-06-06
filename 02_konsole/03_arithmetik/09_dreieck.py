b222f38df4e32b5625f3acbfda5bf0c0828922cb
curdin-bischoff <curdin.bischoff@stud.kswe.ch>
2026-06-06 15:07:26 +0200

    Zinsen

--- 02_konsole/03_arithmetik/09_dreieck.py
+++
@@ -1,6 +0,0 @@
-import random as rd 
-
-g = rd.randrange(10, 1001)
-h = rd.randrange(10, 1001)
-flaeche = g * h/2
-print(f"Flächeninhalt: {flaeche}")
---
+++ 02_konsole/03_arithmetik/09_zinsen.py
@@ -0,0 +1,15 @@
+import random as rd
+
+konto = rd.randrange(100, 1001)
+zinsen_arten = [0.01, 0.025, 0.03, 0.035, 0.04, 0.05]
+zins = rd.choice(zinsen_arten)
+zinssatz = zins*100
+zinsen = zins*konto
+neues_konto = konto + zinsen
+
+print("Jahresende - Kontoauszug")
+print(f"Kontostand: CHF {konto}")
+print(f"Zinssatz: {zinssatz} %")
+print(f"Zinsen: CHF {zinsen}")
+print(f"Neuer Kontostand: CHF {neues_konto}")
+print("Danke für Ihr vertrauen in die Random-Bank")
