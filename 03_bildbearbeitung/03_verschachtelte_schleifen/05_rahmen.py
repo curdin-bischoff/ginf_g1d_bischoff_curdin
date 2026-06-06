0c9d502152b4119a4308151063ae61b0192f5efc
curdin-bischoff <curdin.bischoff@stud.kswe.ch>
2026-06-06 15:59:29 +0200

    Korrigieren Rahmen

--- 03_bildbearbeitung/03_verschachtelte_schleifen/05_rahmen.py
+++ 03_bildbearbeitung/03_verschachtelte_schleifen/05_rahmen.py
@@ -3,8 +3,8 @@ import random as rd
 
 bild = img.new("RGB", (500,500))
 
-r = rd.randrange(0,256)
-g = rd.randrange(0,256)
+r = rd.randrange(0, 256)
+g = rd.randrange(0, 256)
 b = rd.randrange(0, 256)
 
 for y in range(0, bild.height):
@@ -12,8 +12,8 @@ for y in range(0, bild.height):
         bild.putpixel((x, y), (r, g, b))
 for y in range(10, 490):
     for x in range(10, 490):
-        r = rd.randrange(0,256)
-        g = rd.randrange(0,256)
+        r = rd.randrange(0, 256)
+        g = rd.randrange(0, 256)
         b = rd.randrange(0, 256)
         bild.putpixel((x, y), (r, g, b))
 
